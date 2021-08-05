9182c007db5dd09af8716160d38eeb763c40e4a7
Betacom <Betacom@Betacom-PC41N1.homenet.telecomitalia.it>
2021-08-05 10:39:34 +0200

    aggiornamenti BC

---
+++ gestionecorsi/src/main/java/com/git/bc/DocenteBC.java
@@ -0,0 +1,20 @@
+package com.git.bc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.git.architecture.dao.DAOException;
+import com.git.architecture.dao.DocenteDAO;
+import com.git.bc.model.Docente;
+
+public class DocenteBC {
+	private Connection conn;
+	
+	public Docente[] getAll() throws DAOException{
+		try {
+			return DocenteDAO.getFactory().getAll(conn);
+		} catch(SQLException sql) {
+			throw new DAOException(sql);
+		}
+	}
+}
